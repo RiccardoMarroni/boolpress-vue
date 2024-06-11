7a8d3f6300d3a3829a3cafbdba007d1d9bfea7ee
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-06-11 14:34:32 +0200

    add router

--- src/router.js
+++ src/router.js
@@ -1,22 +1,28 @@
 import {createRouter, createWebHistory } from "vue-router";
 
 import AppHome from '.pages/AppHome.vue';
-import PostList from './pages/ProjectList.vue';
+import PostList from './pages/ProjectsView.vue';
 
 
 const router = CreateRouter({
     history: createWebHistory(),
     routes: [
         {
-            path:"/",
-            name:"home",
-            component: AppHome,
+            path: '/',
+            name: 'home',
+            component: HomeView
         },
         {
-            path:"/projects",
-            name:"ptojects",
-            component: PostList,
-        }
+            path: '/projects',
+            name: 'projects',
+            component: ProjectsView
+        },
+        {
+            path: '/project/:slug',
+            name: 'single_project',
+            component: SingleProjectView
+        },
+       
     ]
 })
 
